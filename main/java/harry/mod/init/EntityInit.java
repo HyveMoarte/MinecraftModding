b541cb9fe91f113a08a129fcb31e8bb1d2c720f0
HarryTechRevs <30769752+HarryTechRevs@users.noreply.github.com>
2017-12-14 17:57:46 +0000

    Add files via upload

---
+++ main/java/harry/mod/init/EntityInit.java
@@ -0,0 +1,21 @@
+package harry.mod.init;
+
+import harry.mod.Main;
+import harry.mod.entity.EntityCentaur;
+import harry.mod.util.Reference;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+
+public class EntityInit 
+{
+	public static void registerEntities()
+	{
+		registerEntity("centaur", EntityCentaur.class, Reference.ENTITY_CENTAUR, 50, 11437146, 000000);
+	}
+	
+	private static void registerEntity(String name, Class<? extends Entity> entity, int id, int range, int color1, int color2)
+	{
+		EntityRegistry.registerModEntity(new ResourceLocation(Reference.MODID + ":" + name), entity, name, id, Main.instance, range, 1, true, color1, color2);
+	}
+}
