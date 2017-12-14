d56ae7ca87d47bfd042ea5f0a267f9d248e1f565
HarryTechRevs <30769752+HarryTechRevs@users.noreply.github.com>
2017-12-14 17:58:41 +0000

    Add files via upload

---
+++ main/java/harry/mod/util/handlers/LootTableHandler.java
@@ -0,0 +1,10 @@
+package harry.mod.util.handlers;
+
+import harry.mod.util.Reference;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.LootTableList;
+
+public class LootTableHandler 
+{
+	public static final ResourceLocation CENTAUR = LootTableList.register(new ResourceLocation(Reference.MODID, "centaur"));
+}
--- main/java/harry/mod/util/handlers/RegistryHandler.java
+++ main/java/harry/mod/util/handlers/RegistryHandler.java
@@ -2,6 +2,7 @@ package harry.mod.util.handlers;
 
 import harry.mod.init.BiomeInit;
 import harry.mod.init.BlockInit;
+import harry.mod.init.EntityInit;
 import harry.mod.init.ItemInit;
 import harry.mod.util.interfaces.IHasModel;
 import harry.mod.world.gen.WorldGenCustomOres;
@@ -58,6 +59,14 @@ public class RegistryHandler
 		GameRegistry.registerWorldGenerator(new WorldGenCustomTrees(), 0);
 		
 		BiomeInit.registerBiomes();
+		
+		EntityInit.registerEntities();
+		RenderHandler.registerEntityRenders();
+	}
+	
+	public static void initRegistries()
+	{
+		SoundsHandler.registerSounds();
 	}
 	
 	public static void postInitRegistries()
